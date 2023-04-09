5007c101dfb9f348026fca95105d264b62b4d01d
sobiaayaz123 <ayjukisobu@gmail.com>
2023-04-09 10:21:25 -0700

    initiali project

---
+++ js app.js
@@ -0,0 +1,112 @@
+// DAY 01 - 07 APRIL
+
+// alert("Welcome To Our World")
+
+// var user = prompt('Put Your Email', 'email')
+// alert("Successfully added")
+
+// var arr= [ 'hello' , 'hi']
+// document.write(arr)
+// console.log(arr)
+
+// var banoQabil = "web Developement"
+// document.write(banoQabil)
+//  console.log (banoQabil)
+
+//  var BanoQabil = "Web Developement"
+// document.write(BanoQabil)
+//  console.log (BanoQabil)
+
+//  var bano_Qabil = "web_Developement"
+// document.write(bano_Qabil)
+//  console.log (bano_Qabil)
+
+// var num1 =+ prompt("Num 1 Please")
+// var num2 =+ prompt("Num 2 Please")
+// document.write(popularNumber)
+// console.log(num1+num2)
+
+
+// var popularNumber = 12 - 24;
+// document.write(popularNumber)
+// console.log(12-24)
+
+// var num = 10;
+// var popularNumber = num + 200;
+// document.write(popularNumber)
+// console.log(num + 200)
+
+// var popularNumber = 3 * 12;
+// document.write(popularNumber)
+// console.log(3 * 12)
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// document.write(popularNumber)
+// console.log(num + anotherNum)
+
+// var num = 1;
+// var newNum = num++
+// document.write(newNum)
+// console.log(num ++)
+
+// var num = 1;
+// var newNum = ++num
+// document.write(newNum)
+// console.log(++num)
+
+// var num = 1;
+// var newNum = num--
+// document.write(newNum)
+// console.log(num--)
+
+// var num = 1;
+// var newNum = --num
+// document.write(newNum)
+// console.log(--num)
+
+// var totalCost = 1 + 3 * 4;
+// var totalCost = 1 + (3 * 4);
+// document.write(totalCost)
+// console.log(1 + (3 * 4))
+
+
+// var totalCost = (1 + 3) * 4;
+// document.write(totalCost)
+// console.log((1 + 3) * 4)
+
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// document.write(resultOfComputation)
+// console.log(((2 * 4) * 4) + 2)
+
+
+// resultOfComputation = (2 * 4) * (4 + 2)
+// document.write(resultOfComputation)
+// console.log((2 * 4) * (4 + 2))
+
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+
+
+// DAY 02 - 08APRIL
+
+
+
+
+var zakatPercentage = 0.025;
+userInput = Number( prompt("Enter a number"));
+var result = (zakatPercentage)*(userInput);
+alert ("your zakat value is"+result)
